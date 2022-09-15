8cda94c19bdf3f75d617b3371e03a6f74a562189
Ice-design <ice.design.kh@gmail.com>
2022-09-15 21:11:09 +0300

    test

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,17 @@
 var ICON = 'https://binotel.netlify.app/images/bin.png';
 
 var btnCallback = function (t, opts) {
+    let number = 0;
     t.card("all").then(function (card) {
         console.log(card.customFieldItems);
         console.log(card.customFieldItems[0].value.text);
-        let number = card.customFieldItems[0].value.text;
-        return t.popup({
-            title: 'Call list Binotel',
-            url: './call-list.html',
-            args: { phone: number},
-            height: 278
-        });
+        number = card.customFieldItems[0].value.text;
+    });
+    return t.popup({
+        title: 'Call list Binotel',
+        url: './call-list.html',
+        args: { phone: number},
+        height: 278
     });
 };
 
