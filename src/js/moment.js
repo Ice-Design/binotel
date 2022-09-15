98ba7d59357fe680f987e664c3fdb9ba2c5a8dab
Ice-design <ice.design.kh@gmail.com>
2022-09-15 23:57:14 +0300

    test

--- src/js/moment.js
+++ src/js/moment.js
@@ -12,25 +12,6 @@ const call_list = function(t) {
         return null;
     });
 };
-const fetchWeatherData = function(t) {
-    return t.card('coordinates')
-        .then(function(card) {
-            if (card.coordinates) {
-                const { latitude, longitude } = card.coordinates;
-                // our card has a location, let's fetch the current weather
-                return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`)
-                    .then(function(response) {
-                        return response.json();
-                    })
-                    .then(function(weatherData) {
-                        const freedomUnits = (weatherData.main.temp - 273.15) * 1.8 + 32;
-                        weatherData.main.formattedTemp = `${freedomUnits.toFixed()} °F`;
-                        return weatherData;
-                    });
-            }
-            return null;
-        });
-};
 console.log(call_list);
 
 document.getElementById('auth-btn').addEventListener('click', function(){
