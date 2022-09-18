a9a26448203ba39f5e8dcd6491d86b80527d6bde
Ice-design <ice.design.kh@gmail.com>
2022-09-18 17:48:18 +0300

    test

--- src/js/client.js
+++ src/js/client.js
@@ -1,25 +1,15 @@
-const call_lists = function(t) {
+const call_list = function(t) {
     return t.card('customFieldItems').then(function(card) {
         if (card.customFieldItems[0].value.text) {
             const number = card.customFieldItems[0].value.text;
-            return fetch(`https://burov.fdesign.space/binotel.php?phone=${number}&key=ad063f-bc3c065&secret=2c33b5-b39283-10b289-7e5eea-9bc4ff0c`)
+            return fetch(`https://work.ice-design.pp.ua/binotel.php?phone=${number}&key=ad063f-bc3c065&secret=2c33b5-b39283-10b289-7e5eea-9bc4ff0c`)
                 .then(function(response) {
-                    console.log(response.json());
                     return response.json();
                 })
         }
         return null;
     });
 };
-
-// const call_list = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c67ccb9eff2798ee75b5599b4899ff4b`)
-//     .then(function(response) {
-//         response.json();
-//     });
-const list = fetch(`https://work.ice-design.pp.ua/binotel.php?phone=0969597771&key=ad063f-bc3c065&secret=2c33b5-b39283-10b289-7e5eea-9bc4ff0c`)
-    .then(function(response) {
-        return response.json();
-    });
 const btnCallback = function (t, opts) {
     console.log(list);
     return t.popup({
