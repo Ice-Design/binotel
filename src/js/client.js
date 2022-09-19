35e81cdfebd3d558be6dc0e54a1cc20a6f0e3cc9
Ice-design <ice.design.kh@gmail.com>
2022-09-19 16:04:05 +0300

    test

--- src/js/client.js
+++ src/js/client.js
@@ -1,9 +1,20 @@
+const call_list = function(t) {
+    return t.card("all").then(function (card) {
+        phone = card.customFieldItems[0].value.text;
+        fetch(`https://work.ice-design.pp.ua/binotel.php?phone=${phone}&key=ad063f-bc3c065&secret=2c33b5-b39283-10b289-7e5eea-9bc4ff0c`)
+            .then(function(response) {
+                return response.json();
+            })
+    });
+};
+
 
 const btnCallback = function (t, opts) {
     return t.popup({
         title: 'Call list Binotel',
         url: './call-list.html',
-        height: 278
+        height: 278,
+        args: { myArgs: call_list(t) },
     });
 };
 
