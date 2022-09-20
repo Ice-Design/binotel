653af7afc4b03ce337514f14c6c4781061b82db0
Ice-design <ice.design.kh@gmail.com>
2022-09-20 18:31:36 +0300

    test

--- src/js/moment.js
+++ src/js/moment.js
@@ -1,23 +1,35 @@
-const call_list = function(number) {
-    return fetch(`https://work.ice-design.pp.ua/binotel.php?phone=${number}&key=ad063f-bc3c065&secret=2c33b5-b39283-10b289-7e5eea-9bc4ff0c`)
-    .then(function(response) {
+const t = window.TrelloPowerUp.iframe();
+let number = t.arg('arg');
+const btnCallback = function (number) {
+    let arrays = [];
+    fetch('https://work.ice-design.pp.ua/binotel.php?phone=0679589192&key=ad063f-bc3c065&secret=2c33b5-b39283-10b289-7e5eea-9bc4ff0c')
+    .then((response) => {
         return response.json();
     })
-};
+    .then((data) => {
+        arrays = Object.values(data);
+        for (let i = 0; i < arrays.length; i++) {
+            if (arrays[i]['callType'] != 0){
+                var minutes = parseInt((arrays[i]['billsec'] * 1000) % (1000 * 60 * 60) / (1000 * 60));
+                var seconds = ((arrays[i]['billsec'] * 1000) % (1000 * 60)) / 1000;
+                $('.title_list').after('<li class="num_'+[i]+'"></li>');
+                if (arrays[i]['disposition'] == 'ANSWER'){
+                    $('.num_'+[i]).append($('<b class="yes">Прийнятий</b>'));
+                }else if(arrays[i]['disposition'] == 'CANCEL'){
+                    $('.num_'+[i]).append($('<b class="no">Відхиленний</b>'));
+                }
+                $('.num_'+[i]).append($('<span>', {
+                    'text': '🕐'+minutes+':'+seconds+' хв.'
+                }));
+                $('.num_'+[i]+' span').append($('<span>', { 'text': new Date(arrays[i]['startTime']*1000).toLocaleString("ro-RO")}));
+                $('.num_'+[i]).append($('<button id="'+arrays[i]['callID']+'">📞</button>'));
+            }
+        }
+    });
 
-const t = window.TrelloPowerUp.iframe();
-let phone;
-t.card("all").then(function (card) {
-    phone = card.customFieldItems[0].value.text;
-});
-console.log(call_list(phone));
+};
+console.log(btnCallback(number));
 
 document.getElementById('auth-btn').addEventListener('click', function(){
-    const t = window.TrelloPowerUp.iframe();
-    let phone;
-    t.card("all").then(function (card) {
-        phone = card.customFieldItems[0].value.text;
-    });
-    console.log(phone);
-    console.log(call_list(phone));
+
 });
