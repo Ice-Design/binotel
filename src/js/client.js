8fda49b13a3a5fbed7f7cfd3f1141382422b58e7
Ice-design <ice.design.kh@gmail.com>
2022-09-21 16:36:07 +0300

    test

--- src/js/client.js
+++ src/js/client.js
@@ -4,6 +4,9 @@ const btnCallback = function (t, opts) {
             if (card.customFieldItems[i]['idCustomField'] == '6323289097e72700a118e186'){
                 let phone = card.customFieldItems[i].value.text;
                 phone = phone.replace(/[\D]+/g, '');
+                if (phone.substr(0, 2) == '38'){
+                    phone.slice(2)
+                }
                 return t.popup({
                     title: 'Call list Binotel',
                     url: './call-list.html',
