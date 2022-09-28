ce3350244e06057c9f664f781d0b468d9d2c9f08
Ice-design <ice.design.kh@gmail.com>
2022-09-28 13:30:52 +0300

    test

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,16 +1,16 @@
 const t = window.TrelloPowerUp.iframe();
 
 document.querySelector('.api #key').addEventListener('change', (e) => {
-    t.set('board', 'public', 'key', e.target.value);
+    t.set('board', 'shared', 'key', e.target.value);
 });
 document.querySelector('.api #secret-key').addEventListener('change', (e) => {
-    t.set('board', 'public', 'secret', e.target.value);
+    t.set('board', 'shared', 'secret', e.target.value);
 });
 document.querySelector('.api #bought-key').addEventListener('change', (e) => {
-    t.set('board', 'public', 'bought', e.target.value);
+    t.set('board', 'shared', 'bought', e.target.value);
 });
 document.querySelector('.api #customFields').addEventListener('change', (e) => {
-    t.set('board', 'public', 'customFields', e.target.value);
+    t.set('board', 'shared', 'customFields', e.target.value);
 });
 
 t.render(() => {
@@ -19,19 +19,19 @@ t.render(() => {
         let arrays = board.customFields;
         for (let i = 0; i < arrays.length; i++) {
             jQuery('.api #customFields').append(jQuery("<option></option>", {value: arrays[i]['id'], text: arrays[i]['name']}));
-            if (data.board.public.customFields == arrays[i]['id']) {
+            if (data && data.board.shared.customFields == arrays[i]['id']) {
                 jQuery('.api #customFields option')[i].selected = true;
             }
         }
     });
-    if (data && data.board.public.key) {
-      document.querySelector('.api #key').value = data.board.public.key;
+    if (data && data.board.shared.key) {
+      document.querySelector('.api #key').value = data.board.shared.key;
     }
-    if (data && data.board.public.secret) {
-        document.querySelector(`.api #secret-key`).value = data.board.public.secret;
+    if (data && data.board.shared.secret) {
+        document.querySelector(`.api #secret-key`).value = data.board.shared.secret;
     }
-    if (data && data.board.public.bought) {
-        document.querySelector(`.api #bought-key`).value = data.board.public.bought;
+    if (data && data.board.shared.bought) {
+        document.querySelector(`.api #bought-key`).value = data.board.shared.bought;
     }
     var context = t.getContext();
     document.querySelector(`b.id-bord`).textContent = context.board;
