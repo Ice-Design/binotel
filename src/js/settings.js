d05f67ce704329280f6cc02c8c9cc5d372b5b584
Ice-design <ice.design.kh@gmail.com>
2022-09-28 13:43:15 +0300

    test

--- src/js/settings.js
+++ src/js/settings.js
@@ -16,11 +16,13 @@ document.querySelector('.api #customFields').addEventListener('change', (e) => {
 t.render(() => {
   return t.getAll().then((data) => {
     t.board("all").then(function (board) {
-        let arrays = board.customFields;
-        for (let i = 0; i < arrays.length; i++) {
-            jQuery('.api #customFields').append(jQuery("<option></option>", {value: arrays[i]['id'], text: arrays[i]['name']}));
-            if (data && data.board.shared.customFields == arrays[i]['id']) {
-                jQuery('.api #customFields option')[i].selected = true;
+        if(board.customFields){
+            let arrays = board.customFields;
+            for (let i = 0; i < arrays.length; i++) {
+                jQuery('.api #customFields').append(jQuery("<option></option>", {value: arrays[i]['id'], text: arrays[i]['name']}));
+                if (data && data.board.shared.customFields == arrays[i]['id']) {
+                    jQuery('.api #customFields option')[i].selected = true;
+                }
             }
         }
     });
